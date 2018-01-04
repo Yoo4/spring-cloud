9d5cc2cf625ee4b285d16874af8a0fea86a5078c
wangzhang <wangzhang2016@gmail.com>
2018-01-04 21:34:07 +0800

    add

---
+++ springcloud-learning - 认证完成版，zuul未加过滤器/service-use/src/main/java/com/springcloud/uc/mapper/BaseUserMapper.java
@@ -0,0 +1,7 @@
+package com.springcloud.uc.mapper;
+
+import com.springcloud.uc.entity.BaseUser;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BaseUserMapper extends Mapper<BaseUser> {
+}
