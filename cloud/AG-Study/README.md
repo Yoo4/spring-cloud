d0f872143219c0f672bb6157a7ffe61704277ca7
wangzhang <wangzhang2016@gmail.com>
2018-01-28 22:35:24 +0800

    delete

--- cloud/AG-Study/README.md
+++
@@ -1,2 +0,0 @@
-# AG-Study
-AG-Study
--- cloud/AG-Study/ag-uc/src/main/java/com/github/wxiaoqi/learning/uc/mapper/BaseUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.github.wxiaoqi.learning.uc.mapper;
-
-import com.github.wxiaoqi.learning.uc.entity.BaseUser;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface BaseUserMapper extends Mapper<BaseUser> {
-}
